e827d2b4e4df932eba152d2f40a0b84b3119028e
thauandev <thauandepaula@icloud.com>
2020-03-03 09:58:18 -0300

    Tratando erros de login

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,8 +1,32 @@
-const INITIAL_STATE = {};
+import produce from 'immer';
+
+const INITIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
 
 export default function auth(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_REQUEST': {
+        draft.loading = true;
+        break;
+      }
+
+      case '@auth/SIGN_IN_SUCCESS': {
+        draft.profile = action.payload.user;
+        draft.signed = true;
+        draft.loading = false;
+        break;
+      }
+
+      case '@auth/SIGN_FAILURE': {
+        draft.loading = false;
+        break;
+      }
+
+      default:
+    }
+  });
 }
