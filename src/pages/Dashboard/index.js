9c6e9edcec3d04837e6657af4f2cb514ee37384a
thauandev <thauandepaula@icloud.com>
2020-03-05 16:45:38 -0300

    Implementando as notificações 3

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-
   return <div />;
 }
